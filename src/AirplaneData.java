4e9e75a5530fc2cbcb6e3429cb4c5cfaf394f9de
MrRoscoerocks <roscoe.ambrose@gmail.com>
2017-04-27 14:04:20 -0600

    My Base code - more to follow

---
+++ src/AirplaneData.java
@@ -0,0 +1,37 @@
+
+public class AirplaneData {
+	
+	
+	private String engineType;
+	private String engineLocation;
+	private int engineNumber;
+	
+	
+	
+	
+	public String getEngineType() {
+		return engineType;
+	}
+	public void setEngineType(String engineType) {
+		this.engineType = engineType;
+	}
+	public String getEngineLocation() {
+		return engineLocation;
+	}
+	public void setEngineLocation(String engineLocation) {
+		this.engineLocation = engineLocation;
+	}
+	public int getEngineNumber() {
+		return engineNumber;
+	}
+	public void setEngineNumber(int engineNumber) {
+		this.engineNumber = engineNumber;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
