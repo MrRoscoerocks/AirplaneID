b646b2ec11a4a84aeb47dd0aa02e47f91f10a71d
MrRoscoerocks <roscoe.ambrose@gmail.com>
2017-05-01 13:58:55 -0600

    made a quick change

--- src/Flyer.java
+++ src/Flyer.java
@@ -10,7 +10,7 @@ public class Flyer {
 		
 		
 		AirplaneAnalysis myQuestions = new AirplaneAnalysis();
-		System.out.println("myQuestions Instantiated"); // comment
+		System.out.println("myQuestions Instantiated"); 
 		// with the answers to the above questions, tell which airplane it might be(rule out planes it can't be
 		
 		
